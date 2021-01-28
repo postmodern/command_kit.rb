71a6a86adb700bfd784a737ea4cc16f12dc2d220
Postmodern <postmodern.mod3@gmail.com>
2021-01-28 12:50:43 -0800

    Added Arguments::ArgumentValue.

--- lib/command_kit/arguments/argument.rb
+++ lib/command_kit/arguments/argument.rb
@@ -1,34 +1,14 @@
+require 'command_kit/arguments/argument_value'
+
 module CommandKit
   module Arguments
-    class Argument
-
-      module Name
-        #
-        # Upcases an argument name.
-        #
-        # @param [Symbol] name
-        #
-        # @return [String]
-        #
-        def self.upcase(name)
-          name.to_s.upcase
-        end
-      end
+    class Argument < ArgumentValue
 
       # @return [Symbol]
       attr_reader :name
 
-      # @return [Class, nil]
-      attr_reader :type
-
       # @return [String, nil]
       attr_reader :desc
-
-      # @return [String]
-      attr_reader :usage
-
-      # @return [Object, Proc, nil]
-      attr_reader :default
 
       # @return [Regexp, nil]
       attr_reader :pattern
@@ -59,30 +39,22 @@ module CommandKit
       #
       def initialize(name, type:     String,
                            desc:     ,
-                           usage:    self.class.uage(name),
+                           usage:    name.upcase,
                            default:  nil,
                            required: false,
                            &block)
         @name = name
-        @type = type
         @desc = desc
-        @usage = usage
-        @default = default
-        @required = required
+
+        super(
+          type:     type,
+          usage:    usage,
+          default:  default,
+          required: required
+        )
 
         @pattern, @parser = self.class.optparse(@type)
-      end
-
-      #
-      # The default usage string (ex: `NUM`) for the given argument name
-      # (ex: `:input`).
-      #
-      # @param [Symbol] name
-      #
-      # @return [String]
-      #
-      def self.default_usage(name)
-        Name.upcase(name)
+        @block = block
       end
 
       #
@@ -90,43 +62,6 @@ module CommandKit
       #
       def self.optparse(type)
         OptionParser::DefaultList.search(:atype,type)
-      end
-
-      #
-      # Generates a default usage string for an argument name.
-      #
-      # @param [Symbol] name
-      #   The given argument name.
-      #
-      # @return [String]
-      #   The default usage string.
-      #
-      def self.default_usage(name)
-        if type == String
-          Name.upcase(name)
-        else
-          USAGES[type]
-        end
-      end
-
-      #
-      # Determines if the argument is required or not.
-      #
-      # @return [Boolean]
-      #
-      def required?
-        @required
-      end
-
-      #
-      # The usage string for the argument.
-      #
-      # @return [String, nil]
-      #
-      def usage
-        if required? then @usage
-        elsif @usage then "[#{@usage}]"
-        end
       end
 
       #
---
+++ lib/command_kit/arguments/argument_value.rb
@@ -0,0 +1,63 @@
+module CommandKit
+  module Arguments
+    class ArgumentValue
+      # @return [Class, nil]
+      attr_reader :type
+
+      # @return [Object, Proc, nil]
+      attr_reader :default
+
+      #
+      # Initializes the argument value.
+      #
+      # @param [Class] type
+      #
+      # @param [Boolean] required
+      #
+      # @param [String, nil] usage
+      #
+      # @param [Object, Proc, nil] default
+      #
+      def initialize(type:     nil,
+                     required: true,
+                     default:  nil,
+                     usage:    nil)
+        @type    = type
+        @default = default
+        @usage   = usage
+      end
+
+      #
+      # Determines if the argument is required or not.
+      #
+      # @return [Boolean]
+      #
+      def required?
+        @required
+      end
+
+      #
+      # The usage string for the argument.
+      #
+      # @return [String, nil]
+      #
+      def usage
+        if required? then @usage
+        elsif @usage then "[#{@usage}]"
+        end
+      end
+
+      #
+      # Returns a new default value.
+      #
+      # @return [Object]
+      #
+      def default_value
+        if @default.respond_to?(:call) then @default.call
+        else                                @default.dup
+        end
+      end
+
+    end
+  end
+end
