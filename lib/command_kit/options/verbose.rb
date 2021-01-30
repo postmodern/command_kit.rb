270302db037bb173e5fe8b3efe276702f991e9aa
Postmodern <postmodern.mod3@gmail.com>
2021-01-29 17:12:28 -0800

    Added Options::Verbose.

---
+++ lib/command_kit/options/verbose.rb
@@ -0,0 +1,30 @@
+require 'command_kit/options'
+
+module CommandKit
+  module Options
+    #
+    # Defines a `-v`,`--verbose` option.
+    #
+    module Verbose
+      #
+      # Includes {Options} and defines a `-v, --verbose` option.
+      #
+      def self.included(command)
+        command.include Options
+
+        command.option :verbose, short: '-v', desc: 'Enables verbose output' do
+          @verbose = true
+        end
+      end
+
+      #
+      # Determines if verbose mode is enabled.
+      #
+      # @return [Boolean]
+      #
+      def verbose?
+        @verbose
+      end
+    end
+  end
+end
