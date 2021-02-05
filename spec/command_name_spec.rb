76cc0e68c05b90ffb3a50ddb6b386c18a30b3ab8
Postmodern <postmodern.mod3@gmail.com>
2021-02-04 22:09:58 -0800

    Put test classes in their own namespace.

--- spec/command_name_spec.rb
+++ spec/command_name_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 require 'command_kit/command_name'
 
 describe CommandName do
-  class ImplicitCmd
-    include CommandKit::CommandName
+  module TestCommandName
+    class ImplicitCmd
+      include CommandKit::CommandName
+    end
   end
 
-  let(:command_class) { ImplicitCmd }
+  let(:command_class) { TestCommandName::ImplicitCmd }
 
   describe ".command_name" do
-    subject { ImplicitCmd }
+    subject { command_class }
 
     context "when no command_name has been set" do
       it "should underscore the class'es name" do
@@ -18,12 +20,14 @@ describe CommandName do
     end
 
     context "when a command_name is explicitly set" do
-      class ExplicitCmd
-        include CommandKit::CommandName
-        command_name 'explicit'
+      module TestCommandName
+        class ExplicitCmd
+          include CommandKit::CommandName
+          command_name 'explicit'
+        end
       end
 
-      subject { ExplicitCmd }
+      subject { TestCommandName::ExplicitCmd }
 
       it "must return the explicitly set command_name" do
         expect(subject.command_name).to eq('explicit')
@@ -31,30 +35,34 @@ describe CommandName do
     end
 
     context "when the command class inherites from another class" do
-      class BaseCmd
-        include CommandKit::CommandName
+      module TestCommandName
+        class BaseCmd
+          include CommandKit::CommandName
+        end
+
+        class InheritedCmd < BaseCmd
+        end
       end
 
-      class InheritedCmd < BaseCmd
-      end
-
-      subject { InheritedCmd }
+      subject { TestCommandName::InheritedCmd }
 
       it "should underscore the class'es name" do
         expect(subject.command_name).to eq('inherited_cmd')
       end
 
       context "when the superclass defines an explicit command_name" do
-        class ExplicitBaseCmd
-          include CommandKit::CommandName
-          command_name :explicit
+        module TestCommandName
+          class ExplicitBaseCmd
+            include CommandKit::CommandName
+            command_name :explicit
+          end
+
+          class ImplicitInheritedCmd < ExplicitBaseCmd
+          end
         end
 
-        class ImplicitInheritedCmd < ExplicitBaseCmd
-        end
-
-        let(:super_subject) { ExplicitBaseCmd }
-        subject { ImplicitInheritedCmd }
+        let(:super_subject) { TestCommandName::ExplicitBaseCmd }
+        subject { TestCommandName::ImplicitInheritedCmd }
 
         it "must return the subclass'es command_name, not the superclass'es" do
           expect(subject.command_name).to eq('implicit_inherited_cmd')
@@ -66,16 +74,18 @@ describe CommandName do
       end
 
       context "when the subclass defines an explicit command_name" do
-        class ImplicitBaseCmd
-          include CommandKit::CommandName
+        module TestCommandName
+          class ImplicitBaseCmd
+            include CommandKit::CommandName
+          end
+
+          class ExplicitInheritedCmd < ImplicitBaseCmd
+            command_name :explicit
+          end
         end
 
-        class ExplicitInheritedCmd < ImplicitBaseCmd
-          command_name :explicit
-        end
-
-        let(:super_subject) { ImplicitBaseCmd }
-        subject { ExplicitInheritedCmd }
+        let(:super_subject) { TestCommandName::ImplicitBaseCmd }
+        subject { TestCommandName::ExplicitInheritedCmd }
 
         it "must return the subclass'es command_name, not the superclass'es" do
           expect(subject.command_name).to eq('explicit')
