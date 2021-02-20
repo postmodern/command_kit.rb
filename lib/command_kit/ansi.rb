d703800899a419da17c4407680b02dbb26e9ade8
Postmodern <postmodern.mod3@gmail.com>
2021-02-20 03:44:23 -0800

    Added missing YARDoc to CommandKit::ANSI.

--- lib/command_kit/ansi.rb
+++ lib/command_kit/ansi.rb
@@ -6,128 +6,215 @@ module CommandKit
   #
   module ANSI
 
+    # ANSI color code for black
     BLACK = "\e[30m"
 
+    # ANSI color code for red
     RED = "\e[31m"
 
+    # ANSI color code for green
     GREEN = "\e[32m"
 
+    # ANSI color code for yellow
     YELLOW = "\e[33m"
 
+    # ANSI color code for blue
     BLUE = "\e[34m"
 
+    # ANSI color code for megenta
     MAGENTA = "\e[35m"
 
+    # ANSI color code for cyan
     CYAN = "\e[36m"
 
+    # ANSI color code for white
     WHITE = "\e[37m"
 
+    # ANSI color code for bright black
     BRIGHT_BLACK = "\e[90m"
 
+    # @see BRIGHT_BLACK
     BRIGHT_GRAY = BRIGHT_BLACK
 
+    # ANSI color code for bright red
     BRIGHT_RED = "\e[91m"
 
+    # ANSI color code for bright green
     BRIGHT_GREEN = "\e[92m"
 
+    # ANSI color code for bright yellow
     BRIGHT_YELLOW = "\e[93m"
 
+    # ANSI color code for bright blue
     BRIGHT_BLUE = "\e[94m"
 
+    # ANSI color code for bright megenta
     BRIGHT_MEGENTA = "\e[95m"
 
+    # ANSI color code for bright cyan
     BRIGHT_CYAN = "\e[96m"
 
+    # ANSI color code for bright white
     BRIGHT_WHITE = "\e[97m"
 
+    # ANSI code for bold text
     BOLD = "\e[1m"
 
+    # ANSI reset code
     RESET = "\e[0m"
 
+    # @see RESET
     CLEAR = RESET
 
     module_function
 
+    #
+    # Checks if the stream supports ANSI output.
+    #
+    # @param [IO] stream
+    #
+    # @return [Boolean]
+    #
     def color?(stream)
       ENV['TERM'] != 'dumb' && stream.tty?
     end
 
+    #
+    # @see BLACK
+    #
     def black
       BLACK
     end
 
+    #
+    # @see RED
+    #
     def red
       RED
     end
 
+    #
+    # @see GREEN
+    #
     def green
       GREEN
     end
 
+    #
+    # @see YELLOW
+    #
     def yellow
       YELLOW
     end
 
+    #
+    # @see BLUE
+    #
     def blue
       BLUE
     end
 
+    #
+    # @see MAGENTA
+    #
     def magenta
       MAGENTA
     end
 
+    #
+    # @see CYAN
+    #
     def cyan
       CYAN
     end
 
+    #
+    # @see WHITE
+    #
     def white
       WHITE
     end
 
+    #
+    # @see BRIGHT_BLACK
+    #
     def bright_black
       BRIGHT_BLACK
     end
 
+    #
+    # @see BRIGHT_GRAY
+    #
     def bright_gray
       BRIGHT_GRAY
     end
 
+    #
+    # @see BRIGHT_RED
+    #
     def bright_red
       BRIGHT_RED
     end
 
+    #
+    # @see BRIGHT_GREEN
+    #
     def bright_green
       BRIGHT_GREEN
     end
 
+    #
+    # @see BRIGHT_YELLOW
+    #
     def bright_yellow
       BRIGHT_YELLOW
     end
 
+    #
+    # @see BRIGHT_BLUE
+    #
     def bright_blue
       BRIGHT_BLUE
     end
 
+    #
+    # @see BRIGHT_MEGENTA
+    #
     def bright_magenta
       BRIGHT_MAGENTA
     end
 
+    #
+    # @see BRIGHT_CYAN
+    #
     def bright_cyan
       BRIGHT_CYAN
     end
 
+    #
+    # @see BRIGHT_WHITE
+    #
     def bright_white
       BRIGHT_WHITE
     end
 
+    #
+    # @see BOLD
+    #
     def bold
       BOLD
     end
 
+    #
+    # @see RESET
+    #
     def reset
       RESET
     end
 
+    #
+    # @see CLEAR
+    #
     def clear
       CLEAR
     end
