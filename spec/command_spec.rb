c41de624a83ba6fb9e46a6efba51ed0bebde2a30
Postmodern <postmodern.mod3@gmail.com>
2021-04-02 23:31:09 -0700

    Added specs for Command.

---
+++ spec/command_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'command_kit/command'
+
+describe Command do
+  it "must include CommandKit::Main" do
+    expect(described_class).to include(CommandKit::Main)
+  end
+
+  it "must include CommandKit::Env" do
+    expect(described_class).to include(CommandKit::Env)
+  end
+
+  it "must include CommandKit::Stdio" do
+    expect(described_class).to include(CommandKit::Stdio)
+  end
+
+  it "must include CommandKit::Printing" do
+    expect(described_class).to include(CommandKit::Printing)
+  end
+
+  it "must include CommandKit::Help" do
+    expect(described_class).to include(CommandKit::Help)
+  end
+
+  it "must include CommandKit::Usage" do
+    expect(described_class).to include(CommandKit::Usage)
+  end
+
+  it "must include CommandKit::Arguments" do
+    expect(described_class).to include(CommandKit::Arguments)
+  end
+
+  it "must include CommandKit::Options" do
+    expect(described_class).to include(CommandKit::Options)
+  end
+
+  it "must include CommandKit::Examples" do
+    expect(described_class).to include(CommandKit::Examples)
+  end
+
+  it "must include CommandKit::Description" do
+    expect(described_class).to include(CommandKit::Description)
+  end
+
+  it "must include CommandKit::ExceptionHandler" do
+    expect(described_class).to include(CommandKit::ExceptionHandler)
+  end
+
+end
