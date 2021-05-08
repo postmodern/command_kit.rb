593b9116ac002af3350b37cd478cdb9270917cd4
Postmodern <postmodern.mod3@gmail.com>
2021-05-07 23:28:42 -0700

    Added CommandKit::Env::Path for parsing/searching $PATH env variable.

---
+++ lib/command_kit/env/path.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'command_kit/env'
+
+module CommandKit
+  module Env
+    #
+    # Provides access to the `PATH` environment variable.
+    #
+    # ## Environment Variables
+    #
+    # * `PATH` - The list of directories to search for commands within.
+    #
+    module Path
+      include Env
+
+      # The home directory.
+      #
+      # @return [String]
+      attr_reader :path_dirs
+
+      #
+      # Initializes {#path_dirs} using `env['PATH']`.
+      #
+      # @param [Hash{Symbol => Object}] kwargs
+      #   Additional keyword arguments.
+      #
+      def initialize(**kwargs)
+        super(**kwargs)
+
+        @path_dirs = env.fetch('PATH','').split(File::PATH_SEPARATOR)
+      end
+
+      #
+      # Searches for the command in {#path_dirs}.
+      #
+      # @param [String, Symbol] name
+      #   The command name.
+      #
+      # @return [String, nil]
+      #   The absolute path to the executable file, or `nil` if the command
+      #   could not be found in any of the {#path_dirs}.
+      #
+      def find_command(name)
+        name = name.to_s
+
+        @path_dirs.each do |dir|
+          file = File.join(dir,name)
+
+          return file if File.file?(file) && File.executable?(file)
+        end
+
+        return nil
+      end
+
+      #
+      # Determines if the command is present on the system.
+      #
+      # @param [String, Symbol] name
+      #   The command name.
+      #
+      # @return [Boolean]
+      #   Specifies whether a command with the given name exists in one of the
+      #   {#path_dirs}.
+      #
+      def command_installed?(name)
+        !find_command(name).nil?
+      end
+    end
+  end
+end
---
+++ spec/env/path_spec.rb
@@ -0,0 +1,78 @@
+require 'spec_helper'
+require 'command_kit/env/path'
+
+describe Env::Path do
+  module TestEnvPath
+    class TestCommand
+
+      include CommandKit::Env::Path
+
+    end
+  end
+
+  let(:command_class) { TestEnvPath::TestCommand }
+  subject { command_class.new }
+
+  describe "#initialize" do
+    it "must split ENV['PATH'] into an Array of directories" do
+      expect(subject.path_dirs).to eq(ENV['PATH'].split(File::PATH_SEPARATOR))
+    end
+
+    context "when given a custom 'PATH variable" do
+      let(:path_dirs) { %w[/bin /usr/bin] }
+      let(:path)      { path_dirs.join(File::PATH_SEPARATOR) }
+
+      subject { command_class.new(env: {'PATH' => path}) }
+
+      it "must split env['PATH'] into an Array of directories" do
+        expect(subject.path_dirs).to eq(path_dirs)
+      end
+    end
+
+    context "when given an empty 'PATH' env variable" do
+      subject { command_class.new(env: {'PATH' => ''}) }
+
+      it "must set #path_dirs to []" do
+        expect(subject.path_dirs).to eq([])
+      end
+    end
+  end
+
+  describe "#find_command" do
+    context "when given a valid command name" do
+      it "must search #path_dirs for the command" do
+        command = subject.find_command('ruby')
+
+        expect(subject.path_dirs).to include(File.dirname(command))
+      end
+
+      it "must return the path to the command" do
+        command = subject.find_command('ruby')
+
+        expect(command).to_not be(nil)
+        expect(File.file?(command)).to be(true)
+        expect(File.executable?(command)).to be(true)
+      end
+    end
+
+    context "when given an unknown command name" do
+      it "must return nil" do
+        expect(subject.find_command('does_not_exist')).to be(nil)
+      end
+    end
+  end
+
+  describe "#command_installed?" do
+    context "when given a valid command name" do
+      it do
+        expect(subject.command_installed?('ruby')).to be(true)
+      end
+    end
+
+    context "when given an unknown command name" do
+      it "must return nil" do
+        expect(subject.command_installed?('does_not_exist')).to be(false)
+      end
+    end
+  end
+end
