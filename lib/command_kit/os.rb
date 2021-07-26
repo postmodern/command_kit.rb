40a72f46f1999e9ccfd537c435cbf955cfec49d4
Postmodern <postmodern.mod3@gmail.com>
2021-07-26 02:10:29 -0700

    Added more methods for determining the specific Linux distro.

--- lib/command_kit/os.rb
+++ lib/command_kit/os.rb
@@ -24,10 +24,87 @@ module CommandKit
     #
     # @return [Boolean]
     #
+    # @group Linux
+    #
     # @api public
     #
     def linux?
       RUBY_PLATFORM.include?('linux')
+    end
+
+    #
+    # Determines if the current OS is RedHat Linux based distro.
+    #
+    # @return [Boolean]
+    #
+    # @group Linux
+    #
+    # @api public
+    #
+    # @since 0.2.0
+    #
+    def redhat_linux?
+      linux? && File.file?('/etc/redhat-release')
+    end
+
+    #
+    # Determines if the current OS is Fedora Linux based distro.
+    #
+    # @return [Boolean]
+    #
+    # @group Linux
+    #
+    # @api public
+    #
+    # @since 0.2.0
+    #
+    def fedora_linux?
+      linux? && File.file?('/etc/fedora-release')
+    end
+
+    #
+    # Determines if the current OS is Debian Linux based distro.
+    #
+    # @return [Boolean]
+    #
+    # @group Linux
+    #
+    # @api public
+    #
+    # @since 0.2.0
+    #
+    def debian_linux?
+      linux? && File.file?('/etc/debian_version')
+    end
+
+    #
+    # Determines if the current OS is SuSE Linux based distro.
+    #
+    # @return [Boolean]
+    #
+    # @group Linux
+    #
+    # @api public
+    #
+    # @since 0.2.0
+    #
+    def suse_linux?
+      linux? && File.file?('/etc/SuSE-release')
+    end
+
+    #
+    # Determines if the current OS is Arch Linux based distro.
+    #
+    # @return [Boolean]
+    #
+    # @group Linux
+    #
+    # @api public
+    #
+    # @since 0.2.0
+    #
+    def arch_linux?
+      linux? && File.file?('/etc/arch-release')
     end
 
     #
