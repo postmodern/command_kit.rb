93b83bca80432396d566a96a5c55714ddbd8d4cf
Postmodern <postmodern.mod3@gmail.com>
2021-07-26 04:50:33 -0700

    Refactored CommandKit::OS::Linux to accept a linux_distro: keyword.
    * Moved identification logic into ClassMethods#linux_distro.
    * Added an #initialize method.
    * Convert #linux_distro into an attr_reader.

--- spec/os/linux_spec.rb
+++ spec/os/linux_spec.rb
@@ -2,234 +2,153 @@ require 'spec_helper'
 require 'command_kit/os/linux'
 
 describe CommandKit::OS::Linux do
-  class TestOSLinux
-    include CommandKit::OS::Linux
+  module TestOSLinux
+    class TestCommand
+      include CommandKit::OS::Linux
+    end
   end
 
-  subject { TestOSLinux.new }
+  let(:command_class) { TestOSLinux::TestCommand }
 
-  describe "#redhat_linux?" do
-    context "when RUBY_PLATFORM contains 'linux'" do
-      before { stub_const('RUBY_PLATFORM','x86_64-linux') }
+  describe ".linux_distro" do
+    subject { command_class }
 
-      file = '/etc/redhat-release'
-
-      context "and the #{file} file exists" do
-        before do
-          expect(File).to receive(:file?).with(file).and_return(true)
-        end
-
-        it { expect(subject.redhat_linux?).to be(true) }
+    context "and the /etc/fedora-release file exists" do
+      before do
+        expect(File).to receive(:file?).with("/etc/fedora-release").and_return(true)
       end
 
-      context "but the /etc/redhat-release file does not exists" do
-        before do
-          expect(File).to receive(:file?).with(file).and_return(false)
-        end
-
-        it { expect(subject.redhat_linux?).to be(false) }
-      end
+      it { expect(subject.linux_distro).to be(:fedora) }
     end
 
-    context "when RUBY_PLATFORM does not contain 'linux'" do
-      before { stub_const('RUBY_PLATFORM','aarch64-darwin') }
+    context "and the /etc/redhat-release file exists" do
+      before do
+        allow(File).to receive(:file?).with("/etc/fedora-release").and_return(false)
+        expect(File).to receive(:file?).with("/etc/redhat-release").and_return(true)
+      end
+
+      it { expect(subject.linux_distro).to be(:redhat) }
+    end
+
+    context "and the /etc/debian_version file exists" do
+      before do
+        allow(File).to receive(:file?).with("/etc/fedora-release").and_return(false)
+        allow(File).to receive(:file?).with("/etc/redhat-release").and_return(false)
+        expect(File).to receive(:file?).with("/etc/debian_version").and_return(true)
+      end
+
+      it { expect(subject.linux_distro).to be(:debian) }
+    end
+
+    context "and the /etc/SuSE-release file exists" do
+      before do
+        allow(File).to receive(:file?).with("/etc/fedora-release").and_return(false)
+        allow(File).to receive(:file?).with("/etc/redhat-release").and_return(false)
+        allow(File).to receive(:file?).with("/etc/debian_version").and_return(false)
+        expect(File).to receive(:file?).with("/etc/SuSE-release").and_return(true)
+      end
+
+      it { expect(subject.linux_distro).to be(:suse) }
+    end
+
+    context "and the /etc/arch-release file exists" do
+      before do
+        allow(File).to receive(:file?).with("/etc/fedora-release").and_return(false)
+        allow(File).to receive(:file?).with("/etc/redhat-release").and_return(false)
+        allow(File).to receive(:file?).with("/etc/debian_version").and_return(false)
+        allow(File).to receive(:file?).with("/etc/SuSE-release").and_return(false)
+        expect(File).to receive(:file?).with("/etc/arch-release").and_return(true)
+      end
+
+      it { expect(subject.linux_distro).to be(:arch) }
+    end
+  end
+
+  subject { command_class.new }
+
+  describe "#initialize" do
+    it "must default #linux_distro to self.class.linux_distro" do
+      expect(subject.linux_distro).to eq(command_class.linux_distro)
+    end
+
+    context "when the linux_distro: keyword is given" do
+      let(:linux_distro) { :suse }
+
+      subject { command_class.new(linux_distro: linux_distro) }
+
+      it "must set #linux_distro" do
+        expect(subject.linux_distro).to eq(linux_distro)
+      end
+    end
+  end
+
+  describe "#redhat_linux?" do
+    context "when #linux_distro is :redhat" do
+      subject { command_class.new(linux_distro: :redhat) }
+
+      it { expect(subject.redhat_linux?).to be(true) }
+    end
+
+    context "when #linux_distro is not :redhat" do
+      subject { command_class.new(linux_distro: :debian) }
 
       it { expect(subject.redhat_linux?).to be(false) }
     end
   end
 
   describe "#fedora_linux?" do
-    context "when RUBY_PLATFORM contains 'linux'" do
-      before { stub_const('RUBY_PLATFORM','x86_64-linux') }
+    context "when #linux_distro is :fedora" do
+      subject { command_class.new(linux_distro: :fedora) }
 
-      file = '/etc/fedora-release'
-
-      context "and the #{file} file exists" do
-        before do
-          expect(File).to receive(:file?).with(file).and_return(true)
-        end
-
-        it { expect(subject.fedora_linux?).to be(true) }
-      end
-
-      context "but the #{file} file does not exists" do
-        before do
-          expect(File).to receive(:file?).with(file).and_return(false)
-        end
-
-        it { expect(subject.fedora_linux?).to be(false) }
-      end
+      it { expect(subject.fedora_linux?).to be(true) }
     end
 
-    context "when RUBY_PLATFORM does not contain 'linux'" do
-      before { stub_const('RUBY_PLATFORM','aarch64-darwin') }
+    context "when #linux_distro is not :fedora" do
+      subject { command_class.new(linux_distro: :debian) }
 
       it { expect(subject.fedora_linux?).to be(false) }
     end
   end
 
   describe "#debian_linux?" do
-    context "when RUBY_PLATFORM contains 'linux'" do
-      before { stub_const('RUBY_PLATFORM','x86_64-linux') }
+    context "when #linux_distro is :debian" do
+      subject { command_class.new(linux_distro: :debian) }
 
-      file = '/etc/debian_version'
-
-      context "and the #{file} file exists" do
-        before do
-          expect(File).to receive(:file?).with(file).and_return(true)
-        end
-
-        it { expect(subject.debian_linux?).to be(true) }
-      end
-
-      context "but the #{file} file does not exists" do
-        before do
-          expect(File).to receive(:file?).with(file).and_return(false)
-        end
-
-        it { expect(subject.debian_linux?).to be(false) }
-      end
+      it { expect(subject.debian_linux?).to be(true) }
     end
 
-    context "when RUBY_PLATFORM does not contain 'linux'" do
-      before { stub_const('RUBY_PLATFORM','aarch64-darwin') }
+    context "when #linux_distro is not :fedora" do
+      subject { command_class.new(linux_distro: :redhat) }
 
       it { expect(subject.debian_linux?).to be(false) }
     end
   end
 
   describe "#suse_linux?" do
-    context "when RUBY_PLATFORM contains 'linux'" do
-      before { stub_const('RUBY_PLATFORM','x86_64-linux') }
+    context "when #linux_distro is :suse" do
+      subject { command_class.new(linux_distro: :suse) }
 
-      file = '/etc/SuSE-release'
-
-      context "and the #{file} file exists" do
-        before do
-          expect(File).to receive(:file?).with(file).and_return(true)
-        end
-
-        it { expect(subject.suse_linux?).to be(true) }
-      end
-
-      context "but the #{file} file does not exists" do
-        before do
-          expect(File).to receive(:file?).with(file).and_return(false)
-        end
-
-        it { expect(subject.suse_linux?).to be(false) }
-      end
+      it { expect(subject.suse_linux?).to be(true) }
     end
 
-    context "when RUBY_PLATFORM does not contain 'linux'" do
-      before { stub_const('RUBY_PLATFORM','aarch64-darwin') }
+    context "when #linux_distro is not :suse" do
+      subject { command_class.new(linux_distro: :debian) }
 
       it { expect(subject.suse_linux?).to be(false) }
     end
   end
 
   describe "#arch_linux?" do
-    context "when RUBY_PLATFORM contains 'linux'" do
-      before { stub_const('RUBY_PLATFORM','x86_64-linux') }
+    context "when #linux_distro is :arch" do
+      subject { command_class.new(linux_distro: :arch) }
 
-      file = '/etc/arch-release'
-
-      context "and the #{file} file exists" do
-        before do
-          expect(File).to receive(:file?).with(file).and_return(true)
-        end
-
-        it { expect(subject.arch_linux?).to be(true) }
-      end
-
-      context "but the #{file} file does not exists" do
-        before do
-          expect(File).to receive(:file?).with(file).and_return(false)
-        end
-
-        it { expect(subject.arch_linux?).to be(false) }
-      end
+      it { expect(subject.arch_linux?).to be(true) }
     end
 
-    context "when RUBY_PLATFORM does not contain 'linux'" do
-      before { stub_const('RUBY_PLATFORM','aarch64-darwin') }
+    context "when #linux_distro is not :arch" do
+      subject { command_class.new(linux_distro: :debian) }
 
       it { expect(subject.arch_linux?).to be(false) }
-    end
-  end
-
-  describe "#linux_distro" do
-    context "when RUBY_PLATFORM contains 'linux'" do
-      before { stub_const('RUBY_PLATFORM','x86_64-linux') }
-
-      context "when both the /etc/redhat-release and /etc/fedora-release files exist" do
-        before do
-          allow(File).to receive(:file?).with('/etc/redhat-release').and_return(true)
-          allow(File).to receive(:file?).with('/etc/fedora-release').and_return(true)
-        end
-
-        it { expect(subject.linux_distro).to eq(:fedora) }
-      end
-
-      context "when the /etc/redhat-release file exists" do
-        before do
-          allow(File).to receive(:file?).with('/etc/redhat-release').and_return(true)
-          allow(File).to receive(:file?).with('/etc/fedora-release').and_return(false)
-        end
-
-        it { expect(subject.linux_distro).to eq(:redhat) }
-      end
-
-      context "when the /etc/debian_version file exists" do
-        before do
-          allow(File).to receive(:file?).with('/etc/redhat-release').and_return(false)
-          allow(File).to receive(:file?).with('/etc/fedora-release').and_return(false)
-          allow(File).to receive(:file?).with('/etc/debian_version').and_return(true)
-        end
-
-        it { expect(subject.linux_distro).to eq(:debian) }
-      end
-
-      context "when the /etc/SuSE-release file exists" do
-        before do
-          allow(File).to receive(:file?).with('/etc/redhat-release').and_return(false)
-          allow(File).to receive(:file?).with('/etc/fedora-release').and_return(false)
-          allow(File).to receive(:file?).with('/etc/debian_version').and_return(false)
-          allow(File).to receive(:file?).with('/etc/SuSE-release').and_return(true)
-        end
-
-        it { expect(subject.linux_distro).to eq(:suse) }
-      end
-
-      context "when the /etc/arch-release file exists" do
-        before do
-          allow(File).to receive(:file?).with('/etc/redhat-release').and_return(false)
-          allow(File).to receive(:file?).with('/etc/fedora-release').and_return(false)
-          allow(File).to receive(:file?).with('/etc/debian_version').and_return(false)
-          allow(File).to receive(:file?).with('/etc/SuSE-release').and_return(false)
-          allow(File).to receive(:file?).with('/etc/arch-release').and_return(true)
-        end
-
-        it { expect(subject.linux_distro).to eq(:arch) }
-      end
-
-      context "when none of the files exist" do
-        before do
-          allow(File).to receive(:file?).with('/etc/redhat-release').and_return(false)
-          allow(File).to receive(:file?).with('/etc/fedora-release').and_return(false)
-          allow(File).to receive(:file?).with('/etc/debian_version').and_return(false)
-          allow(File).to receive(:file?).with('/etc/SuSE-release').and_return(false)
-          allow(File).to receive(:file?).with('/etc/arch-release').and_return(false)
-        end
-
-        it { expect(subject.linux_distro).to be(nil) }
-      end
-    end
-
-    context "when RUBY_PLATFORM does not contain 'linux'" do
-      before { stub_const('RUBY_PLATFORM','aarch64-darwin') }
-
-      it { expect(subject.linux_distro).to be(nil) }
     end
   end
 end
