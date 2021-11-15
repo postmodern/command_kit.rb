3b38604a0523541199263e55e78368bac548bb8f
Postmodern <postmodern.mod3@gmail.com>
2021-11-15 04:27:36 -0800

    Added missing specs for OptionValue.default_usage.

--- spec/options/option_value_spec.rb
+++ spec/options/option_value_spec.rb
@@ -7,6 +7,61 @@ describe CommandKit::Options::OptionValue do
   let(:required) { true }
   let(:default)  { 1 }
 
+  describe ".default_usage" do
+    subject { described_class }
+
+    context "when given a Hash" do
+      let(:type) do
+        {'foo' => :foo, "bar" => :bar}
+      end
+
+      it "must join the Hash's keys with a '|' character" do
+        expect(subject.default_usage(type)).to eq(type.keys.join('|'))
+      end
+    end
+
+    context "when given an Array" do
+      let(:type) do
+        ['foo', 'bar', 'baz']
+      end
+
+      it "must join the Array's elements with a '|' character" do
+        expect(subject.default_usage(type)).to eq(type.join('|'))
+      end
+    end
+
+    context "when given a Regexp" do
+      let(:type) { /[0-9a-f]+/ }
+
+      it "must return the Regexp's source" do
+        expect(subject.default_usage(type)).to eq(type.source)
+      end
+    end
+
+    context "when given a Class" do
+      module TestOptionValue
+        class FooBarBaz
+        end
+      end
+
+      let(:type) { TestOptionValue::FooBarBaz }
+
+      it "must return the uppercase and underscored version of it's name" do
+        expect(subject.default_usage(type)).to eq("FOO_BAR_BAZ")
+      end
+    end
+
+    context "when given another kind of Object" do
+      let(:type) { Object.new }
+
+      it do
+        expect {
+          subject.default_usage(type)
+        }.to raise_error(TypeError,"unsupported option type: #{type.inspect}")
+      end
+    end
+  end
+
   describe "#initialize" do
     context "when the type: keyword is given" do
       let(:type) { Integer }
