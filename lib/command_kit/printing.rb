4490c1991dba5b6ba2f0ec173d181df34aa7a9a1
Postmodern <postmodern.mod3@gmail.com>
2022-06-29 22:25:53 -0700

    Added `Printing#print_fields`.

--- lib/command_kit/printing.rb
+++ lib/command_kit/printing.rb
@@ -62,5 +62,41 @@ module CommandKit
     def print_exception(error)
       print_error error.full_message(highlight: stderr.tty?)
     end
+
+    #
+    # Prints a Hash as left-justified `:` separated fields.
+    #
+    # @param [Hash, Array<(Object, Object)>] fields
+    #   The fields to print.
+    #
+    # @example
+    #   print_fields('Name' => 'foo', 'Version' => '0.1.0')
+    #   # Name:    foo
+    #   # Version: 0.1.0
+    #
+    # @api public
+    #
+    # @since 0.4.0
+    #
+    def print_fields(fields)
+      max_length = 0
+
+      fields = fields.map { |name,value|
+        name       = name.to_s
+        value      = value.to_s
+        max_length = name.length if name.length > max_length
+
+        [name, value]
+      }
+
+      max_length += 2
+
+      fields.each do |name,value|
+        print "#{name}:".ljust(max_length)
+        puts value
+      end
+
+      return nil
+    end
   end
 end
