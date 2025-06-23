ee30c2cb01e1eb234da128ad9bd952c55a8a5f8d
Postmodern <postmodern.mod3@gmail.com>
2025-06-22 17:16:54 -0700

    Refactored `CommandKit::Colors::PlainText` to not use meta-programming.

--- lib/command_kit/colors/plain_text.rb
+++ lib/command_kit/colors/plain_text.rb
@@ -1,35 +1,329 @@
 # frozen_string_literal: true
 
-require_relative 'ansi'
-
 module CommandKit
   module Colors
     #
-    # Dummy module with the same interface as {ANSI}, but for when ANSI is not
-    # supported.
+    # Dummy module that does not apply any ANSI formatting to text, for when
+    # stdout is redirected to a file or piped to another command.
     #
     module PlainText
-      ANSI.constants(false).each do |name|
-        const_set(name,'')
-      end
+      # @!macro dummy_constant
+      #   Dummy constant for API compatibility with {ANSI}.
+
+      # @!macro dummy_constant
+      RESET = ''
+
+      # @!macro dummy_constant
+      CLEAR = ''
+
+      # @!macro dummy_constant
+      BOLD = ''
+
+      # @!macro dummy_constant
+      RESET_INTENSITY = ''
+
+      # @!macro dummy_constant
+      BLACK = ''
+
+      # @!macro dummy_constant
+      RED = ''
+
+      # @!macro dummy_constant
+      GREEN = ''
+
+      # @!macro dummy_constant
+      YELLOW = ''
+
+      # @!macro dummy_constant
+      BLUE = ''
+
+      # @!macro dummy_constant
+      MAGENTA = ''
+
+      # @!macro dummy_constant
+      CYAN = ''
+
+      # @!macro dummy_constant
+      WHITE = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      BRIGHT_BLACK = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      BRIGHT_RED = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      BRIGHT_GREEN = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      BRIGHT_YELLOW = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      BRIGHT_BLUE = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      BRIGHT_MAGENTA = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      BRIGHT_CYAN = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      BRIGHT_WHITE = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      RESET_FG = ''
+
+      # @!macro dummy_constant
+      # @see RESET_FG
+      RESET_COLOR = ''
+
+      # @!macro dummy_constant
+      # @since 0.2.0
+      ON_BLACK = ''
+
+      # @!macro dummy_constant
+      # @since 0.2.0
+      ON_RED = ''
+
+      # @!macro dummy_constant
+      # @since 0.2.0
+      ON_GREEN = ''
+
+      # @!macro dummy_constant
+      # @since 0.2.0
+      ON_YELLOW = ''
+
+      # @!macro dummy_constant
+      # @since 0.2.0
+      ON_BLUE = ''
+
+      # @!macro dummy_constant
+      # @since 0.2.0
+      ON_MAGENTA = ''
+
+      # @!macro dummy_constant
+      # @since 0.2.0
+      ON_CYAN = ''
+
+      # @!macro dummy_constant
+      # @since 0.2.0
+      ON_WHITE = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      ON_BRIGHT_BLACK = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      ON_BRIGHT_RED = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      ON_BRIGHT_GREEN = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      ON_BRIGHT_YELLOW = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      ON_BRIGHT_BLUE = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      ON_BRIGHT_MAGENTA = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      ON_BRIGHT_CYAN = ''
+
+      # @!macro dummy_constant
+      # @since 0.3.0
+      ON_BRIGHT_WHITE = ''
+
+      # @!macro dummy_constant
+      # @since 0.2.0
+      RESET_BG = ''
 
       module_function
 
-      def reset = RESET
+      #
+      # Dummy method for API compatibility with {ANSI}.
+      #
+      # @return [String]
+      #   An empty string.
+      #
+      def reset = ''
 
-      def clear = reset
+      #
+      # Dummy method for API compatibility with {ANSI}.
+      #
+      # @return [String]
+      #   An empty string.
+      #
+      def clear = ''
 
-      [
-        :bold,
-        :black, :red, :green, :yellow, :blue, :magenta, :cyan, :white,
-        :bright_black, :gray, :bright_red, :bright_green, :bright_yellow, :bright_blue, :bright_magenta, :bright_cyan, :bright_white,
-        :on_black, :on_red, :on_green, :on_yellow, :on_blue, :on_magenta, :on_cyan, :on_white,
-        :on_bright_black, :on_gray, :on_bright_red, :on_bright_green, :on_bright_yellow, :on_bright_blue, :on_bright_magenta, :on_bright_cyan, :on_bright_white
-      ].each do |name|
-        define_method(name) do |string=nil|
-          string || ''
-        end
-      end
+      #
+      # @!macro dummy_method
+      #   Dummy method for API compatibility with {ANSI}.
+      #
+      #   @param [String, nil] string
+      #     The string that would normally be ANSI highlighted.
+      #
+      #   @return [String]
+      #     The unchanged string or an empty string if no string was given.
+      #
+      #   @see ANSI.$0
+      #   @api public
+      #
+
+      # @!macro dummy_method
+      def bold(string=nil) = string || ''
+
+      #
+      # @group Foreground Color Methods
+      #
+
+      # @!macro dummy_method
+      def black(string=nil) = string || ''
+
+      # @!macro dummy_method
+      def red(string=nil) = string || ''
+
+      # @!macro dummy_method
+      def green(string=nil) = string || ''
+
+      # @!macro dummy_method
+      def yellow(string=nil) = string || ''
+
+      # @!macro dummy_method
+      def blue(string=nil) = string || ''
+
+      # @!macro dummy_method
+      def magenta(string=nil) = string || ''
+
+      # @!macro dummy_method
+      def cyan(string=nil) = string || ''
+
+      # @!macro dummy_method
+      def white(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def bright_black(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def gray(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def bright_red(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def bright_green(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def bright_yellow(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def bright_blue(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def bright_magenta(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def bright_cyan(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def bright_white(string=nil) = string || ''
+
+      #
+      # @group Background Color Methods
+      #
+
+      # @!macro dummy_method
+      # @since 0.2.0
+      def on_black(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.2.0
+      def on_red(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.2.0
+      def on_green(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.2.0
+      def on_yellow(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.2.0
+      def on_blue(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.2.0
+      def on_magenta(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.2.0
+      def on_cyan(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.2.0
+      def on_white(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def on_bright_black(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def on_gray(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def on_bright_red(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def on_bright_green(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def on_bright_yellow(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def on_bright_blue(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def on_bright_magenta(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def on_bright_cyan(string=nil) = string || ''
+
+      # @!macro dummy_method
+      # @since 0.3.0
+      def on_bright_white(string=nil) = string || ''
     end
   end
 end
